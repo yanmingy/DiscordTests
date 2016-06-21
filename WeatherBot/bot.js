174b9ab062c02bf22a57c70079f8f3942465ea26
yanmingy <>
2016-06-21 14:00:56 -0500

    Added more functionality including shortcuts, name changes, and message to all. Included a help function.

--- WeatherBot/bot.js
+++ WeatherBot/bot.js
@@ -9,13 +9,15 @@ var wbot = new Discord.Client();
 wbot.loginWithToken("MTk0NTM1NjI4NDgwNzA4NjA5.CknW7A.cyeEpC16MtQzU7qagUJZLJC27SM");
 
 var destroy = false;
+var idle = false;
+var help = "\nEmote Commands:\n!yum\n!gib\n!shrug\n!fat\n!kyu\n!lennyface\n!sadface\n!ugh\n!fatlenny\n!retard\n!comfort\n\nFunctions:\nSet game: !playing [game]\nGo idle (no response): !idle\nGo online (change from idle): !online\nChange username: !username [new name]\nCurrent Weather: !weather [zip or city]\n5 Day Forecast: !forecast [zip or city]\nRelog: !relog\nShut down bot: !destroy\nMessage to all Channels: !messageall [message]\n"
 
 //var chan = new Discord.Channel();
 //On connection, say hi!
 //Client.server is a cache of servers -> Cache of Server Channels 
 wbot.on("ready", function(){
 	console.log("Bot has connected");
-	sendToAll("Ym-Bot has connected");
+	sendToAll("Ym-Bot has connected. \nFor a list of commands type !ihavedown");
 });
 
 //On message functions
@@ -23,28 +25,119 @@ wbot.on("message", function(message) {
 
 	var content = message.content;
 
-    if(content === "kek") {
-        wbot.reply(message, "lenny");
-    }
+	if(content ==="!ihavedown"){
+		wbot.reply(message,help);
+	}
 
-    if(content === "!relog"){
-    	wbot.logout();
-    }
+	if(!idle){
 
-    if(content === "!destroy"){
-    	wbot.destroy();
-    	destroy = true;
-    }
+		//console.log(message.author+" "+wbot.user);
 
-    //!weather
-    if(content.substr(0,8)=="!weather"){
-    	getWeather(message);
-    }
+		//shortcuts
+	    if(content === "kek") {
+	        wbot.reply(message, "lenny");
+	    }
 
-    //!forecast
-    if(content.substr(0,9)=="!forecast"){
-    	getForecast(message);
-    }
+	    if((content.search("ramen") != -1 || content.search("Ramen") !=-1) && message.author != wbot.user){
+	    	wbot.reply(message, "One ramen please (      ͡      ͜ʖ      ͡     )");
+	    }
+
+	    if(content === "!yum"){
+	    	wbot.sendMessage(message.channel, "ლ(´ڡ`ლ)");
+	    }
+
+	    if(content === "!gib"){
+	    	wbot.sendMessage(message.channel, "༼ つ ◕_◕ ༽つ");
+	    }
+
+	    if(content === "!shrug"){
+	    	wbot.sendMessage(message.channel, "¯\\_(ツ)_/¯");
+	    }
+
+	    if(content === "!fat"){
+	    	wbot.sendMessage(message.channel, "(￣ー￣)");
+	    }
+
+	    if(content === "!kyu"){
+	    	wbot.sendMessage(message.channel, "／人◕ ‿‿ ◕人＼﻿");
+	    }
+
+	    if(content === "!lennyface"){
+	    	wbot.sendMessage(message.channel, "( ͡° ͜ʖ ͡°)");
+	    }
+
+	    if(content === "!sadface"){
+	    	wbot.sendMessage(message.channel, "(°ʖ̯°)");
+	    }
+
+	    if(content === "!ugh"){
+	    	wbot.sendMessage(message.channel, "( ͡° ʖ̯ ͡°)");
+	    }
+
+	    if(content === "!fatlenny"){
+	    	wbot.sendMessage(message.channel, "(      ͡      ͜ʖ      ͡     )");
+	    }
+
+	    if(content === "!retard"){
+	    	wbot.sendMessage(message.channel, "ヽ(。_°)ノ");
+	    }
+
+	    if(content === "!comfort"){
+	    	wbot.sendMessage(message.channel, "( T_T)＼(^-^ )");
+	    }
+
+	    //Set playing message
+	    if (content.substr(0,8) == "!playing"){
+	    	wbot.setPlayingGame(content.substr(9,content.length));
+	    }
+
+	    //Set status to idle. The bot will now ignore everything except !online.
+	    if(content === "!idle"){
+	    	wbot.setStatusIdle();
+	    	idle = true;
+	    }
+
+	    //!weather
+	    if(content.substr(0,8)=="!weather"){
+	    	getWeather(message);
+	    }
+
+
+	    //Set username
+	    if(content.substr(0,9) == "!username"){
+	    	wbot.setUsername(content.substr(10,content.length));
+	    }
+
+	    //!forecast
+	    if(content.substr(0,9)=="!forecast"){
+	    	getForecast(message);
+	    }
+
+	    //Sends a message to all channels
+	    if(content.substr(0,11) == "!messageall"){
+	    	sendToAll(content.substr(12,content.length));
+	    }
+
+	}
+	else{
+		//Goes online (able to function properly)
+		if(content === "!online"){
+			wbot.setStatusOnline();
+			idle = false;
+		}
+	}
+
+	//Global access regardless of status
+	if(content === "!relog"){
+	    wbot.logout();
+	}
+
+	if(content === "!destroy"){
+	    wbot.destroy();
+	    destroy = true;
+	}
+
+
 
 });
 
@@ -113,7 +206,7 @@ function getWeather(message){
 function getForecast(message){
 
 	var location = message.content;
-	location = location.substr(9,location.length);
+	location = location.substr(10,location.length);
 
 	weather.find({search: location, degreeType: 'F'}, function(err, result) {
 	  if(err){
