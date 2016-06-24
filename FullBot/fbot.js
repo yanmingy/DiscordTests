243e30bcb3c48df39fd75b04d3af7a3c9c70c7d9
yanmingy <ym.yang7@gmail.com>
2016-06-24 15:33:06 -0500

    Added testing functionality to discord.js bot for sound.

--- FullBot/fbot.js
+++ FullBot/fbot.js
@@ -1,13 +1,48 @@
 
 var Discord = require("discord.js");
 
-var mybot = new Discord.Client();
+var fbot = new Discord.Client();
+var voice;
 
-mybot.on("message", function (message) {
-    if (message.content === "ping") {
-        mybot.reply(message, "pong");
+fbot.on("ready", function () {
+    console.log("Bot has connected");
+    for (i = 0; i < fbot.servers.length; i++) {
+        var channel = fbot.servers[i].channels;
+        for (j = 0; j < channel.length; j++) {
+            if (channel[j].type === 'voice') {
+
+                fbot.joinVoiceChannel(channel[j], function (err, connection) {
+
+                    if (err) {
+                        console.log("There was an error in your shitty code.");
+                        console.log(err);
+                    }
+                    else {
+                        voice = connection;
+                    }
+
+                });
+                return;
+            }
+
+        }
     }
 });
 
-mybot.loginWithToken("MTk1OTg0ODM5MzQwNDU3OTg1.Ck8cmw.UYOOP3-iaxaBPeLvXljO6t3FAvs");
-// If you still need to login with email and password, use mybot.login("email", "password");
+
+
+fbot.on("message", function (message) {
+    if (message.content === "ping") {
+        fbot.reply(message, "pong");
+    }
+
+    if (voice != undefined) {
+        console.log("playing");
+        voice.setVolume(0.25);
+        voice.playFile("http://mp3komplit.wapka.mobi/music/down/45802342/2692558/NjBiNHN2RndUV0lod2YzUXE0UmVMSDVKdytGd3RudU44WWYzZEQ5bUNnN1JmN0xyUGc=/SISTAR+-+I+Like+That.mp3");
+    }
+
+
+});
+
+fbot.loginWithToken("MTk1OTg0ODM5MzQwNDU3OTg1.Ck8cmw.UYOOP3-iaxaBPeLvXljO6t3FAvs");
